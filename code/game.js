bfed4db205f906ac17164c106179f724ed79d0ee
spizura <33125856+spizura@users.noreply.github.com>
2017-10-26 12:55:55 -0400

    platformer

--- code/game.js
+++ code/game.js
@@ -26,7 +26,7 @@ function Level(plan) {
       else if (ch == "!")
         fieldType = "lava";
 
-      else if (ch == "y");
+      else if (ch == "y")
         fieldType = "floater";
 
       // "Push" the fieldType, which is a string, onto the gridLine array (at the end).
